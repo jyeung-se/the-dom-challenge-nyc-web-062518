882ad0cb7a4ab96d8be412c37627536ff620aa2e
Jack Yeung <jyeung.se@gmail.com>
2018-08-19 15:23:13 -0400

    Done.

--- challenge.js
+++ challenge.js
@@ -1,16 +1,63 @@
-let plus1 = document.getElementById("+");
-let minus1 = document.getElementById("-");
+const counter = document.getElementById("counter");
+const likeButton = document.getElementById("+");
+const unlikeButton = document.getElementById("-");
+const pauseButton = document.getElementById("pause");
+const heartButton = document.getElementById("<3");
+const commentList = document.getElementById("list");
+const commentFormBody = document.getElementById("comment-form-body");
+const submit = document.getElementById("submit");
+const likes = document.querySelector(".likes");
 
 
-function increment() {
-  return ++document.getElementById("counter").innerText;
+
+function incrementLike() {
+  counter.innerText = parseInt(counter.innerText) + 1;
+}
+
+function decrementLike() {
+  if (parseInt(counter.innerText) > 0) {
+    counter.innerText = parseInt(counter.innerText) - 1;
+  }
+}
+
+function addComment(commentBodyText) {
+  let comment = document.createElement("p")
+  comment.innerText = commentBodyText
+  commentList.appendChild(comment)
 }
 
 
-function decrement() {
-  let number = finalNumber.innerHTML;
-  number--;
-  finalNumber.innerHTML = number;
+function pauseEventListeners() {
+  clearInterval(autoLikeIncrement)
+  likeButton.removeEventListener("click", incrementLike)
+  unlikeButton.removeEventListener("click", decrementLike)
+  submit.disabled = true
 }
 
-plus1.addEventListener("click", increment)
+function resumeEventListener() {
+  autoLikeIncrement = setInterval(() => { incrementLike() }, 1000)
+  likeButton.addEventListener("click", incrementLike)
+  unlikeButton.addEventListener("click", decrementLike)
+  submit.disabled = false
+}
+
+let autoLikeIncrement = setInterval(() => { incrementLike() }, 1000)
+
+likeButton.addEventListener("click", incrementLike)
+unlikeButton.addEventListener("click", decrementLike)
+// heartButton.addEventListener("click", )
+submit.addEventListener("click", (e) => {
+  e.preventDefault()
+  addComment(commentFormBody.value)
+  commentFormBody.value = ""
+})
+
+pause.addEventListener("click", () => {
+  if (pauseButton.innerText === "pause") {
+    pauseButton.innerText = "resume"
+    pauseEventListeners()
+  } else {
+    pauseButton.innerText = "pause"
+    resumeEventListener()
+  }
+})
